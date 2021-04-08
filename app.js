2bd1c0d611ebf7509c4bcb8e3e0a961c1d35e4b4
pmin825 <pmin.0825@gmail.com>
2021-04-08 12:30:30 -0700

    add get post delete routes

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+
 const db = require("./config/keys").mongoURI;
+const questions = require("./routes/api/questions");
+
+app.use(express.json());
 
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -9,6 +13,7 @@ mongoose
     .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("this works!"));
+app.use("/api/questions", questions);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is serving on port ${port}`));
